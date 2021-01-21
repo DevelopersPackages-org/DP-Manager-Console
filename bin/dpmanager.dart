8e0d20ec8ea72605bacbd8915c79dcbc3b9e085b
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2021-01-22 03:16:00 +0900

    ug: dpmanager - 中途

--- bin/dpmanager.dart
+++ bin/dpmanager.dart
@@ -1,13 +1,54 @@
+import 'package:args/args.dart';
+
+ArgResults argResults;
+
 void main(List<String> args) {
+  ArgParser parserB = ArgParser(allowTrailingOptions: true);
+  final List<ConsoleCmd> cmds = makeCmds();
+  final Argperser parser = addArgCmd(cmds, parser);
+  argResults = parser.parse(arguments);
   if (args.length>0) {
-    for (var arg in args) {
+    for (String arg in args) {
       print(arg);
     }
-  }else{
+  }
+  else{
     print("Hello DPManager");
   }
 }
-
+ArgParser addArgCmd(List<ConSoleCmd> cmds, ArgParser parser){
+  return parser;
+}
+List<ConsoleCmd> makeCmds(){
+  List<ConsoleCmd> cmds = List();
+  cmds.add(Consolecmd());
+  
+  return cmds;
+}
 class ConsoleCmd{
   String cmdname = "";
+  ArgParser parser = ArgParser(allowTrailingOptions: true);
+
+  ConsoleCmd(String name){
+    this.cmdname = name;
+  }
+
+  String get getName => cmdname;
+  ArgParser get getChild => parser;
+
+  void addFlag(Siring name,String abbr,bool defaults,String help){
+    this.parser.addFlag(name, abbr:abbr, defaultsTo: defaults);
+  }
+  void addOpt(){
+    this.parser
+  }
+
+}
+class ConsoleCmd{
+  String flgname = "";
+
+  ConsoleCmd(){
+
+  }
+
 }
--- lib/src/DPManagerConsole_base.dart
+++ lib/src/DPManagerConsole_base.dart
@@ -1,4 +1,5 @@
 // TODO: Put public facing types in this file.
+import 'package:pub_sub/pub_sub.dart' as pub_sub;
 
 /// Checks if you are awesome. Spoiler: you are.
 class Awesome {
